24b90e62d75f50f3602424bbb3a72153e8c01b07
SatoMomoya <goodragon2158@gmail.com>
2019-11-01 09:23:22 +0900

    cure hands angle

---
+++ LastResisters/Intermediate/Build/Android/UE4/Inc/LastResisters/AI1_TaskNodes_GoToNextStance.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef LASTRESISTERS_AI1_TaskNodes_GoToNextStance_generated_h
+#error "AI1_TaskNodes_GoToNextStance.generated.h already included, missing '#pragma once' in AI1_TaskNodes_GoToNextStance.h"
+#endif
+#define LASTRESISTERS_AI1_TaskNodes_GoToNextStance_generated_h
+
+#define LastResisters_Source_LastResisters_AI_AI1_AI1_TaskNodes_AI1_TaskNodes_GoToNextStance_h_15_RPC_WRAPPERS
+#define LastResisters_Source_LastResisters_AI_AI1_AI1_TaskNodes_AI1_TaskNodes_GoToNextStance_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define LastResisters_Source_LastResisters_AI_AI1_AI1_TaskNodes_AI1_TaskNodes_GoToNextStance_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUAI1_TaskNodes_GoToNextStance(); \
+	friend struct Z_Construct_UClass_UAI1_TaskNodes_GoToNextStance_Statics; \
+public: \
+	DECLARE_CLASS(UAI1_TaskNodes_GoToNextStance, UBTTaskNode, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/LastResisters"), NO_API) \
+	DECLARE_SERIALIZER(UAI1_TaskNodes_GoToNextStance)
+
+
+#define LastResisters_Source_LastResisters_AI_AI1_AI1_TaskNodes_AI1_TaskNodes_GoToNextStance_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesUAI1_TaskNodes_GoToNextStance(); \
+	friend struct Z_Construct_UClass_UAI1_TaskNodes_GoToNextStance_Statics; \
+public: \
+	DECLARE_CLASS(UAI1_TaskNodes_GoToNextStance, UBTTaskNode, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/LastResisters"), NO_API) \
+	DECLARE_SERIALIZER(UAI1_TaskNodes_GoToNextStance)
+
+
+#define LastResisters_Source_LastResisters_AI_AI1_AI1_TaskNodes_AI1_TaskNodes_GoToNextStance_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UAI1_TaskNodes_GoToNextStance(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UAI1_TaskNodes_GoToNextStance) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UAI1_TaskNodes_GoToNextStance); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UAI1_TaskNodes_GoToNextStance); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UAI1_TaskNodes_GoToNextStance(UAI1_TaskNodes_GoToNextStance&&); \
+	NO_API UAI1_TaskNodes_GoToNextStance(const UAI1_TaskNodes_GoToNextStance&); \
+public:
+
+
+#define LastResisters_Source_LastResisters_AI_AI1_AI1_TaskNodes_AI1_TaskNodes_GoToNextStance_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UAI1_TaskNodes_GoToNextStance(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UAI1_TaskNodes_GoToNextStance(UAI1_TaskNodes_GoToNextStance&&); \
+	NO_API UAI1_TaskNodes_GoToNextStance(const UAI1_TaskNodes_GoToNextStance&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UAI1_TaskNodes_GoToNextStance); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UAI1_TaskNodes_GoToNextStance); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UAI1_TaskNodes_GoToNextStance)
+
+
+#define LastResisters_Source_LastResisters_AI_AI1_AI1_TaskNodes_AI1_TaskNodes_GoToNextStance_h_15_PRIVATE_PROPERTY_OFFSET
+#define LastResisters_Source_LastResisters_AI_AI1_AI1_TaskNodes_AI1_TaskNodes_GoToNextStance_h_12_PROLOG
+#define LastResisters_Source_LastResisters_AI_AI1_AI1_TaskNodes_AI1_TaskNodes_GoToNextStance_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	LastResisters_Source_LastResisters_AI_AI1_AI1_TaskNodes_AI1_TaskNodes_GoToNextStance_h_15_PRIVATE_PROPERTY_OFFSET \
+	LastResisters_Source_LastResisters_AI_AI1_AI1_TaskNodes_AI1_TaskNodes_GoToNextStance_h_15_RPC_WRAPPERS \
+	LastResisters_Source_LastResisters_AI_AI1_AI1_TaskNodes_AI1_TaskNodes_GoToNextStance_h_15_INCLASS \
+	LastResisters_Source_LastResisters_AI_AI1_AI1_TaskNodes_AI1_TaskNodes_GoToNextStance_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define LastResisters_Source_LastResisters_AI_AI1_AI1_TaskNodes_AI1_TaskNodes_GoToNextStance_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	LastResisters_Source_LastResisters_AI_AI1_AI1_TaskNodes_AI1_TaskNodes_GoToNextStance_h_15_PRIVATE_PROPERTY_OFFSET \
+	LastResisters_Source_LastResisters_AI_AI1_AI1_TaskNodes_AI1_TaskNodes_GoToNextStance_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	LastResisters_Source_LastResisters_AI_AI1_AI1_TaskNodes_AI1_TaskNodes_GoToNextStance_h_15_INCLASS_NO_PURE_DECLS \
+	LastResisters_Source_LastResisters_AI_AI1_AI1_TaskNodes_AI1_TaskNodes_GoToNextStance_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> LASTRESISTERS_API UClass* StaticClass<class UAI1_TaskNodes_GoToNextStance>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID LastResisters_Source_LastResisters_AI_AI1_AI1_TaskNodes_AI1_TaskNodes_GoToNextStance_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ LastResisters/Intermediate/Build/Android/UE4/Inc/LastResisters/PlayerUI.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef LASTRESISTERS_PlayerUI_generated_h
+#error "PlayerUI.generated.h already included, missing '#pragma once' in PlayerUI.h"
+#endif
+#define LASTRESISTERS_PlayerUI_generated_h
+
+#define LastResisters_Source_LastResisters_UI_MainUI_PlayerUI_h_22_RPC_WRAPPERS
+#define LastResisters_Source_LastResisters_UI_MainUI_PlayerUI_h_22_RPC_WRAPPERS_NO_PURE_DECLS
+#define LastResisters_Source_LastResisters_UI_MainUI_PlayerUI_h_22_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUPlayerUI(); \
+	friend struct Z_Construct_UClass_UPlayerUI_Statics; \
+public: \
+	DECLARE_CLASS(UPlayerUI, UUserWidget, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/LastResisters"), NO_API) \
+	DECLARE_SERIALIZER(UPlayerUI)
+
+
+#define LastResisters_Source_LastResisters_UI_MainUI_PlayerUI_h_22_INCLASS \
+private: \
+	static void StaticRegisterNativesUPlayerUI(); \
+	friend struct Z_Construct_UClass_UPlayerUI_Statics; \
+public: \
+	DECLARE_CLASS(UPlayerUI, UUserWidget, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/LastResisters"), NO_API) \
+	DECLARE_SERIALIZER(UPlayerUI)
+
+
+#define LastResisters_Source_LastResisters_UI_MainUI_PlayerUI_h_22_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UPlayerUI(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UPlayerUI) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UPlayerUI); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UPlayerUI); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UPlayerUI(UPlayerUI&&); \
+	NO_API UPlayerUI(const UPlayerUI&); \
+public:
+
+
+#define LastResisters_Source_LastResisters_UI_MainUI_PlayerUI_h_22_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UPlayerUI(UPlayerUI&&); \
+	NO_API UPlayerUI(const UPlayerUI&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UPlayerUI); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UPlayerUI); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UPlayerUI)
+
+
+#define LastResisters_Source_LastResisters_UI_MainUI_PlayerUI_h_22_PRIVATE_PROPERTY_OFFSET
+#define LastResisters_Source_LastResisters_UI_MainUI_PlayerUI_h_19_PROLOG
+#define LastResisters_Source_LastResisters_UI_MainUI_PlayerUI_h_22_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	LastResisters_Source_LastResisters_UI_MainUI_PlayerUI_h_22_PRIVATE_PROPERTY_OFFSET \
+	LastResisters_Source_LastResisters_UI_MainUI_PlayerUI_h_22_RPC_WRAPPERS \
+	LastResisters_Source_LastResisters_UI_MainUI_PlayerUI_h_22_INCLASS \
+	LastResisters_Source_LastResisters_UI_MainUI_PlayerUI_h_22_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define LastResisters_Source_LastResisters_UI_MainUI_PlayerUI_h_22_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	LastResisters_Source_LastResisters_UI_MainUI_PlayerUI_h_22_PRIVATE_PROPERTY_OFFSET \
+	LastResisters_Source_LastResisters_UI_MainUI_PlayerUI_h_22_RPC_WRAPPERS_NO_PURE_DECLS \
+	LastResisters_Source_LastResisters_UI_MainUI_PlayerUI_h_22_INCLASS_NO_PURE_DECLS \
+	LastResisters_Source_LastResisters_UI_MainUI_PlayerUI_h_22_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> LASTRESISTERS_API UClass* StaticClass<class UPlayerUI>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID LastResisters_Source_LastResisters_UI_MainUI_PlayerUI_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
