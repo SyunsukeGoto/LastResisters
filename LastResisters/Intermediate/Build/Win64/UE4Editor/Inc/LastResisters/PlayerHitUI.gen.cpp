437a519fe114f9e87e3f602190e7f92138337cf4
SatoMomoya <goodragon2158@gmail.com>
2019-11-11 11:23:17 +0900

    make enemy Sheld Material

---
+++ LastResisters/Intermediate/Build/Win64/UE4Editor/Inc/LastResisters/PlayerHitUI.gen.cpp
@@ -0,0 +1,280 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "LastResisters/UI/PlayerUI/PlayerHitUI/PlayerHitUI.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodePlayerHitUI() {}
+// Cross Module References
+	LASTRESISTERS_API UClass* Z_Construct_UClass_UPlayerHitUI_NoRegister();
+	LASTRESISTERS_API UClass* Z_Construct_UClass_UPlayerHitUI();
+	UMG_API UClass* Z_Construct_UClass_UUserWidget();
+	UPackage* Z_Construct_UPackage__Script_LastResisters();
+	UMG_API UClass* Z_Construct_UClass_UImage_NoRegister();
+	ENGINE_API UClass* Z_Construct_UClass_UTexture_NoRegister();
+	ENGINE_API UClass* Z_Construct_UClass_UMaterialInterface_NoRegister();
+// End Cross Module References
+	void UPlayerHitUI::StaticRegisterNativesUPlayerHitUI()
+	{
+	}
+	UClass* Z_Construct_UClass_UPlayerHitUI_NoRegister()
+	{
+		return UPlayerHitUI::StaticClass();
+	}
+	struct Z_Construct_UClass_UPlayerHitUI_Statics
+	{
+		static UObject* (*const DependentSingletons[])();
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
+#endif
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam NewProp_BackgroundFive_MetaData[];
+#endif
+		static const UE4CodeGen_Private::FObjectPropertyParams NewProp_BackgroundFive;
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam NewProp_BackgroundFour_MetaData[];
+#endif
+		static const UE4CodeGen_Private::FObjectPropertyParams NewProp_BackgroundFour;
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam NewProp_BackgroundThree_MetaData[];
+#endif
+		static const UE4CodeGen_Private::FObjectPropertyParams NewProp_BackgroundThree;
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam NewProp_BackgroundTwo_MetaData[];
+#endif
+		static const UE4CodeGen_Private::FObjectPropertyParams NewProp_BackgroundTwo;
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam NewProp_BackgroundOne_MetaData[];
+#endif
+		static const UE4CodeGen_Private::FObjectPropertyParams NewProp_BackgroundOne;
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam NewProp_HitImageFive_MetaData[];
+#endif
+		static const UE4CodeGen_Private::FObjectPropertyParams NewProp_HitImageFive;
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam NewProp_HitImageFour_MetaData[];
+#endif
+		static const UE4CodeGen_Private::FObjectPropertyParams NewProp_HitImageFour;
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam NewProp_HitImageThree_MetaData[];
+#endif
+		static const UE4CodeGen_Private::FObjectPropertyParams NewProp_HitImageThree;
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam NewProp_HitImageTwo_MetaData[];
+#endif
+		static const UE4CodeGen_Private::FObjectPropertyParams NewProp_HitImageTwo;
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam NewProp_HitImageOne_MetaData[];
+#endif
+		static const UE4CodeGen_Private::FObjectPropertyParams NewProp_HitImageOne;
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam NewProp_MainTexture_MetaData[];
+#endif
+		static const UE4CodeGen_Private::FObjectPropertyParams NewProp_MainTexture;
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam NewProp_MainMaterial_MetaData[];
+#endif
+		static const UE4CodeGen_Private::FObjectPropertyParams NewProp_MainMaterial;
+		static const UE4CodeGen_Private::FPropertyParamsBase* const PropPointers[];
+		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
+		static const UE4CodeGen_Private::FClassParams ClassParams;
+	};
+	UObject* (*const Z_Construct_UClass_UPlayerHitUI_Statics::DependentSingletons[])() = {
+		(UObject* (*)())Z_Construct_UClass_UUserWidget,
+		(UObject* (*)())Z_Construct_UPackage__Script_LastResisters,
+	};
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_UPlayerHitUI_Statics::Class_MetaDataParams[] = {
+		{ "Comment", "/**\n *\n */" },
+		{ "IncludePath", "UI/PlayerUI/PlayerHitUI/PlayerHitUI.h" },
+		{ "ModuleRelativePath", "UI/PlayerUI/PlayerHitUI/PlayerHitUI.h" },
+		{ "ObjectInitializerConstructorDeclared", "" },
+	};
+#endif
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_BackgroundFive_MetaData[] = {
+		{ "BindWidget", "" },
+		{ "Category", "PlayerHitUI" },
+		{ "Comment", "//Generate some things so we can spawn them or something\n" },
+		{ "EditInline", "true" },
+		{ "ModuleRelativePath", "UI/PlayerUI/PlayerHitUI/PlayerHitUI.h" },
+		{ "ToolTip", "Generate some things so we can spawn them or something" },
+	};
+#endif
+	const UE4CodeGen_Private::FObjectPropertyParams Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_BackgroundFive = { "BackgroundFive", nullptr, (EPropertyFlags)0x001000000008001c, UE4CodeGen_Private::EPropertyGenFlags::Object, RF_Public|RF_Transient|RF_MarkAsNative, 1, STRUCT_OFFSET(UPlayerHitUI, BackgroundFive), Z_Construct_UClass_UImage_NoRegister, METADATA_PARAMS(Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_BackgroundFive_MetaData, ARRAY_COUNT(Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_BackgroundFive_MetaData)) };
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_BackgroundFour_MetaData[] = {
+		{ "BindWidget", "" },
+		{ "Category", "PlayerHitUI" },
+		{ "Comment", "//Generate some things so we can spawn them or something\n" },
+		{ "EditInline", "true" },
+		{ "ModuleRelativePath", "UI/PlayerUI/PlayerHitUI/PlayerHitUI.h" },
+		{ "ToolTip", "Generate some things so we can spawn them or something" },
+	};
+#endif
+	const UE4CodeGen_Private::FObjectPropertyParams Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_BackgroundFour = { "BackgroundFour", nullptr, (EPropertyFlags)0x001000000008001c, UE4CodeGen_Private::EPropertyGenFlags::Object, RF_Public|RF_Transient|RF_MarkAsNative, 1, STRUCT_OFFSET(UPlayerHitUI, BackgroundFour), Z_Construct_UClass_UImage_NoRegister, METADATA_PARAMS(Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_BackgroundFour_MetaData, ARRAY_COUNT(Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_BackgroundFour_MetaData)) };
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_BackgroundThree_MetaData[] = {
+		{ "BindWidget", "" },
+		{ "Category", "PlayerHitUI" },
+		{ "Comment", "//Generate some things so we can spawn them or something\n" },
+		{ "EditInline", "true" },
+		{ "ModuleRelativePath", "UI/PlayerUI/PlayerHitUI/PlayerHitUI.h" },
+		{ "ToolTip", "Generate some things so we can spawn them or something" },
+	};
+#endif
+	const UE4CodeGen_Private::FObjectPropertyParams Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_BackgroundThree = { "BackgroundThree", nullptr, (EPropertyFlags)0x001000000008001c, UE4CodeGen_Private::EPropertyGenFlags::Object, RF_Public|RF_Transient|RF_MarkAsNative, 1, STRUCT_OFFSET(UPlayerHitUI, BackgroundThree), Z_Construct_UClass_UImage_NoRegister, METADATA_PARAMS(Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_BackgroundThree_MetaData, ARRAY_COUNT(Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_BackgroundThree_MetaData)) };
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_BackgroundTwo_MetaData[] = {
+		{ "BindWidget", "" },
+		{ "Category", "PlayerHitUI" },
+		{ "Comment", "//Generate some things so we can spawn them or something\n" },
+		{ "EditInline", "true" },
+		{ "ModuleRelativePath", "UI/PlayerUI/PlayerHitUI/PlayerHitUI.h" },
+		{ "ToolTip", "Generate some things so we can spawn them or something" },
+	};
+#endif
+	const UE4CodeGen_Private::FObjectPropertyParams Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_BackgroundTwo = { "BackgroundTwo", nullptr, (EPropertyFlags)0x001000000008001c, UE4CodeGen_Private::EPropertyGenFlags::Object, RF_Public|RF_Transient|RF_MarkAsNative, 1, STRUCT_OFFSET(UPlayerHitUI, BackgroundTwo), Z_Construct_UClass_UImage_NoRegister, METADATA_PARAMS(Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_BackgroundTwo_MetaData, ARRAY_COUNT(Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_BackgroundTwo_MetaData)) };
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_BackgroundOne_MetaData[] = {
+		{ "BindWidget", "" },
+		{ "Category", "PlayerHitUI" },
+		{ "Comment", "//Generate some things so we can spawn them or something\n" },
+		{ "EditInline", "true" },
+		{ "ModuleRelativePath", "UI/PlayerUI/PlayerHitUI/PlayerHitUI.h" },
+		{ "ToolTip", "Generate some things so we can spawn them or something" },
+	};
+#endif
+	const UE4CodeGen_Private::FObjectPropertyParams Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_BackgroundOne = { "BackgroundOne", nullptr, (EPropertyFlags)0x001000000008001c, UE4CodeGen_Private::EPropertyGenFlags::Object, RF_Public|RF_Transient|RF_MarkAsNative, 1, STRUCT_OFFSET(UPlayerHitUI, BackgroundOne), Z_Construct_UClass_UImage_NoRegister, METADATA_PARAMS(Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_BackgroundOne_MetaData, ARRAY_COUNT(Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_BackgroundOne_MetaData)) };
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_HitImageFive_MetaData[] = {
+		{ "BindWidget", "" },
+		{ "Category", "PlayerHitUI" },
+		{ "Comment", "//Generate some things so we can spawn them or something\n" },
+		{ "EditInline", "true" },
+		{ "ModuleRelativePath", "UI/PlayerUI/PlayerHitUI/PlayerHitUI.h" },
+		{ "ToolTip", "Generate some things so we can spawn them or something" },
+	};
+#endif
+	const UE4CodeGen_Private::FObjectPropertyParams Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_HitImageFive = { "HitImageFive", nullptr, (EPropertyFlags)0x001000000008001c, UE4CodeGen_Private::EPropertyGenFlags::Object, RF_Public|RF_Transient|RF_MarkAsNative, 1, STRUCT_OFFSET(UPlayerHitUI, HitImageFive), Z_Construct_UClass_UImage_NoRegister, METADATA_PARAMS(Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_HitImageFive_MetaData, ARRAY_COUNT(Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_HitImageFive_MetaData)) };
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_HitImageFour_MetaData[] = {
+		{ "BindWidget", "" },
+		{ "Category", "PlayerHitUI" },
+		{ "Comment", "//Generate some things so we can spawn them or something\n" },
+		{ "EditInline", "true" },
+		{ "ModuleRelativePath", "UI/PlayerUI/PlayerHitUI/PlayerHitUI.h" },
+		{ "ToolTip", "Generate some things so we can spawn them or something" },
+	};
+#endif
+	const UE4CodeGen_Private::FObjectPropertyParams Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_HitImageFour = { "HitImageFour", nullptr, (EPropertyFlags)0x001000000008001c, UE4CodeGen_Private::EPropertyGenFlags::Object, RF_Public|RF_Transient|RF_MarkAsNative, 1, STRUCT_OFFSET(UPlayerHitUI, HitImageFour), Z_Construct_UClass_UImage_NoRegister, METADATA_PARAMS(Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_HitImageFour_MetaData, ARRAY_COUNT(Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_HitImageFour_MetaData)) };
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_HitImageThree_MetaData[] = {
+		{ "BindWidget", "" },
+		{ "Category", "PlayerHitUI" },
+		{ "Comment", "//Generate some things so we can spawn them or something\n" },
+		{ "EditInline", "true" },
+		{ "ModuleRelativePath", "UI/PlayerUI/PlayerHitUI/PlayerHitUI.h" },
+		{ "ToolTip", "Generate some things so we can spawn them or something" },
+	};
+#endif
+	const UE4CodeGen_Private::FObjectPropertyParams Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_HitImageThree = { "HitImageThree", nullptr, (EPropertyFlags)0x001000000008001c, UE4CodeGen_Private::EPropertyGenFlags::Object, RF_Public|RF_Transient|RF_MarkAsNative, 1, STRUCT_OFFSET(UPlayerHitUI, HitImageThree), Z_Construct_UClass_UImage_NoRegister, METADATA_PARAMS(Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_HitImageThree_MetaData, ARRAY_COUNT(Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_HitImageThree_MetaData)) };
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_HitImageTwo_MetaData[] = {
+		{ "BindWidget", "" },
+		{ "Category", "PlayerHitUI" },
+		{ "Comment", "//Generate some things so we can spawn them or something\n" },
+		{ "EditInline", "true" },
+		{ "ModuleRelativePath", "UI/PlayerUI/PlayerHitUI/PlayerHitUI.h" },
+		{ "ToolTip", "Generate some things so we can spawn them or something" },
+	};
+#endif
+	const UE4CodeGen_Private::FObjectPropertyParams Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_HitImageTwo = { "HitImageTwo", nullptr, (EPropertyFlags)0x001000000008001c, UE4CodeGen_Private::EPropertyGenFlags::Object, RF_Public|RF_Transient|RF_MarkAsNative, 1, STRUCT_OFFSET(UPlayerHitUI, HitImageTwo), Z_Construct_UClass_UImage_NoRegister, METADATA_PARAMS(Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_HitImageTwo_MetaData, ARRAY_COUNT(Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_HitImageTwo_MetaData)) };
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_HitImageOne_MetaData[] = {
+		{ "BindWidget", "" },
+		{ "Category", "PlayerHitUI" },
+		{ "Comment", "//Generate some things so we can spawn them or something\n" },
+		{ "EditInline", "true" },
+		{ "ModuleRelativePath", "UI/PlayerUI/PlayerHitUI/PlayerHitUI.h" },
+		{ "ToolTip", "Generate some things so we can spawn them or something" },
+	};
+#endif
+	const UE4CodeGen_Private::FObjectPropertyParams Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_HitImageOne = { "HitImageOne", nullptr, (EPropertyFlags)0x001000000008001c, UE4CodeGen_Private::EPropertyGenFlags::Object, RF_Public|RF_Transient|RF_MarkAsNative, 1, STRUCT_OFFSET(UPlayerHitUI, HitImageOne), Z_Construct_UClass_UImage_NoRegister, METADATA_PARAMS(Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_HitImageOne_MetaData, ARRAY_COUNT(Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_HitImageOne_MetaData)) };
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_MainTexture_MetaData[] = {
+		{ "Category", "PlayerHitUI" },
+		{ "Comment", "//Texture we'll set as a parameter to our dynamic material instance\n" },
+		{ "ModuleRelativePath", "UI/PlayerUI/PlayerHitUI/PlayerHitUI.h" },
+		{ "ToolTip", "Texture we'll set as a parameter to our dynamic material instance" },
+	};
+#endif
+	const UE4CodeGen_Private::FObjectPropertyParams Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_MainTexture = { "MainTexture", nullptr, (EPropertyFlags)0x0010000000010001, UE4CodeGen_Private::EPropertyGenFlags::Object, RF_Public|RF_Transient|RF_MarkAsNative, 1, STRUCT_OFFSET(UPlayerHitUI, MainTexture), Z_Construct_UClass_UTexture_NoRegister, METADATA_PARAMS(Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_MainTexture_MetaData, ARRAY_COUNT(Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_MainTexture_MetaData)) };
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_MainMaterial_MetaData[] = {
+		{ "Category", "PlayerHitUI" },
+		{ "Comment", "//Our base material that will be the template for our material instance\n" },
+		{ "ModuleRelativePath", "UI/PlayerUI/PlayerHitUI/PlayerHitUI.h" },
+		{ "ToolTip", "Our base material that will be the template for our material instance" },
+	};
+#endif
+	const UE4CodeGen_Private::FObjectPropertyParams Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_MainMaterial = { "MainMaterial", nullptr, (EPropertyFlags)0x0010000000010001, UE4CodeGen_Private::EPropertyGenFlags::Object, RF_Public|RF_Transient|RF_MarkAsNative, 1, STRUCT_OFFSET(UPlayerHitUI, MainMaterial), Z_Construct_UClass_UMaterialInterface_NoRegister, METADATA_PARAMS(Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_MainMaterial_MetaData, ARRAY_COUNT(Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_MainMaterial_MetaData)) };
+	const UE4CodeGen_Private::FPropertyParamsBase* const Z_Construct_UClass_UPlayerHitUI_Statics::PropPointers[] = {
+		(const UE4CodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_BackgroundFive,
+		(const UE4CodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_BackgroundFour,
+		(const UE4CodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_BackgroundThree,
+		(const UE4CodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_BackgroundTwo,
+		(const UE4CodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_BackgroundOne,
+		(const UE4CodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_HitImageFive,
+		(const UE4CodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_HitImageFour,
+		(const UE4CodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_HitImageThree,
+		(const UE4CodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_HitImageTwo,
+		(const UE4CodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_HitImageOne,
+		(const UE4CodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_MainTexture,
+		(const UE4CodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_UPlayerHitUI_Statics::NewProp_MainMaterial,
+	};
+	const FCppClassTypeInfoStatic Z_Construct_UClass_UPlayerHitUI_Statics::StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<UPlayerHitUI>::IsAbstract,
+	};
+	const UE4CodeGen_Private::FClassParams Z_Construct_UClass_UPlayerHitUI_Statics::ClassParams = {
+		&UPlayerHitUI::StaticClass,
+		nullptr,
+		&StaticCppClassTypeInfo,
+		DependentSingletons,
+		nullptr,
+		Z_Construct_UClass_UPlayerHitUI_Statics::PropPointers,
+		nullptr,
+		ARRAY_COUNT(DependentSingletons),
+		0,
+		ARRAY_COUNT(Z_Construct_UClass_UPlayerHitUI_Statics::PropPointers),
+		0,
+		0x00B010A0u,
+		METADATA_PARAMS(Z_Construct_UClass_UPlayerHitUI_Statics::Class_MetaDataParams, ARRAY_COUNT(Z_Construct_UClass_UPlayerHitUI_Statics::Class_MetaDataParams))
+	};
+	UClass* Z_Construct_UClass_UPlayerHitUI()
+	{
+		static UClass* OuterClass = nullptr;
+		if (!OuterClass)
+		{
+			UE4CodeGen_Private::ConstructUClass(OuterClass, Z_Construct_UClass_UPlayerHitUI_Statics::ClassParams);
+		}
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(UPlayerHitUI, 2802404050);
+	template<> LASTRESISTERS_API UClass* StaticClass<UPlayerHitUI>()
+	{
+		return UPlayerHitUI::StaticClass();
+	}
+	static FCompiledInDefer Z_CompiledInDefer_UClass_UPlayerHitUI(Z_Construct_UClass_UPlayerHitUI, &UPlayerHitUI::StaticClass, TEXT("/Script/LastResisters"), TEXT("UPlayerHitUI"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(UPlayerHitUI);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
