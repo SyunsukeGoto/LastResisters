571ef16c3bdf982ed8def4aeab20131f24b9577b
SatoMomoya <goodragon2158@gmail.com>
2019-11-13 16:37:06 +0900

    add smoke Effect

---
+++ LastResisters/Intermediate/Build/Win64/UE4Editor/Inc/LastResisters/CrackedEdge.generated.h
@@ -0,0 +1,27 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef LASTRESISTERS_CrackedEdge_generated_h
+#error "CrackedEdge.generated.h already included, missing '#pragma once' in CrackedEdge.h"
+#endif
+#define LASTRESISTERS_CrackedEdge_generated_h
+
+#define LastRegister2_LastResisters_Source_LastResisters_UI_EnemyUI_CrackEdges_CrackedEdge_h_15_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FCrackedEdge_Statics; \
+	LASTRESISTERS_API static class UScriptStruct* StaticStruct();
+
+
+template<> LASTRESISTERS_API UScriptStruct* StaticStruct<struct FCrackedEdge>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID LastRegister2_LastResisters_Source_LastResisters_UI_EnemyUI_CrackEdges_CrackedEdge_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ LastResisters/Intermediate/Build/Win64/UE4Editor/Inc/LastResisters/InstanceUpdater.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef LASTRESISTERS_InstanceUpdater_generated_h
+#error "InstanceUpdater.generated.h already included, missing '#pragma once' in InstanceUpdater.h"
+#endif
+#define LASTRESISTERS_InstanceUpdater_generated_h
+
+#define LastRegister2_LastResisters_Source_LastResisters_InstanceUpdater_h_12_RPC_WRAPPERS
+#define LastRegister2_LastResisters_Source_LastResisters_InstanceUpdater_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define LastRegister2_LastResisters_Source_LastResisters_InstanceUpdater_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAInstanceUpdater(); \
+	friend struct Z_Construct_UClass_AInstanceUpdater_Statics; \
+public: \
+	DECLARE_CLASS(AInstanceUpdater, AActor, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/LastResisters"), NO_API) \
+	DECLARE_SERIALIZER(AInstanceUpdater)
+
+
+#define LastRegister2_LastResisters_Source_LastResisters_InstanceUpdater_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAInstanceUpdater(); \
+	friend struct Z_Construct_UClass_AInstanceUpdater_Statics; \
+public: \
+	DECLARE_CLASS(AInstanceUpdater, AActor, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/LastResisters"), NO_API) \
+	DECLARE_SERIALIZER(AInstanceUpdater)
+
+
+#define LastRegister2_LastResisters_Source_LastResisters_InstanceUpdater_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AInstanceUpdater(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AInstanceUpdater) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AInstanceUpdater); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AInstanceUpdater); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AInstanceUpdater(AInstanceUpdater&&); \
+	NO_API AInstanceUpdater(const AInstanceUpdater&); \
+public:
+
+
+#define LastRegister2_LastResisters_Source_LastResisters_InstanceUpdater_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AInstanceUpdater(AInstanceUpdater&&); \
+	NO_API AInstanceUpdater(const AInstanceUpdater&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AInstanceUpdater); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AInstanceUpdater); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AInstanceUpdater)
+
+
+#define LastRegister2_LastResisters_Source_LastResisters_InstanceUpdater_h_12_PRIVATE_PROPERTY_OFFSET
+#define LastRegister2_LastResisters_Source_LastResisters_InstanceUpdater_h_9_PROLOG
+#define LastRegister2_LastResisters_Source_LastResisters_InstanceUpdater_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	LastRegister2_LastResisters_Source_LastResisters_InstanceUpdater_h_12_PRIVATE_PROPERTY_OFFSET \
+	LastRegister2_LastResisters_Source_LastResisters_InstanceUpdater_h_12_RPC_WRAPPERS \
+	LastRegister2_LastResisters_Source_LastResisters_InstanceUpdater_h_12_INCLASS \
+	LastRegister2_LastResisters_Source_LastResisters_InstanceUpdater_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define LastRegister2_LastResisters_Source_LastResisters_InstanceUpdater_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	LastRegister2_LastResisters_Source_LastResisters_InstanceUpdater_h_12_PRIVATE_PROPERTY_OFFSET \
+	LastRegister2_LastResisters_Source_LastResisters_InstanceUpdater_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	LastRegister2_LastResisters_Source_LastResisters_InstanceUpdater_h_12_INCLASS_NO_PURE_DECLS \
+	LastRegister2_LastResisters_Source_LastResisters_InstanceUpdater_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> LASTRESISTERS_API UClass* StaticClass<class AInstanceUpdater>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID LastRegister2_LastResisters_Source_LastResisters_InstanceUpdater_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
